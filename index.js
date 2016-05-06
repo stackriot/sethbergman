341b5b25876e54575fe1fde5db3e9fe0e0188c5a
Seth Bergman <seth.atxwebs@gmail.com>
2016-05-05 23:27:35 -0500

    done!

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var favicon = require('serve-favicon');
 var app = express();
 
 app.use('/assets', express.static(__dirname + '/assets'));
-//app.use(favicon(__dirname + '/favicon.png'));
-var _favicon = favicon(__dirname + '/favicon.png');
+//app.use(favicon(__dirname + '/assets/images/favicon.png'));
+var _favicon = favicon(__dirname + '/assets/images/favicon.png');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -12,7 +12,9 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
-
+app.get('/favicon.png', function(request, response) {
+  response.render('pages/index');
+});
 
 var server = app.listen(process.env.PORT | 5000, function () {
   console.log('Server running at http://0.0.0.0:' + server.address().port)
